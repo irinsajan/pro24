4da992710a4e6fcff6b6cbb6e3b2b0917019b70e
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-11-11 15:38:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,10 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 var ball,ground,box1,box2,box3;
-function preload()
-{
-	
-}
+
 
 function setup() {
 	createCanvas(1500,700);
@@ -17,13 +14,12 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-	//ball = new Ball(200,200,5);
-	ground = new Ground(750,height,1600,20);
+	ball = new Ball(200,200,10);
+	ground = new Ground(750,height,1500,20);
 	box1 = new Dustbin(1415,690,70,20);
 	box2 = new Dustbin(1450,650,20,100);
 	box3 = new Dustbin(1390,650,20,100);
 
-	//keyPressed ();
 
 	Engine.run(engine);
   
@@ -31,17 +27,18 @@ function setup() {
 
 
 function draw() {
-rectMode(CENTER);
+
 background(0);
-drawSprites();
+
 box1.display();
 box2.display();
 box3.display();
 ground.display();
+ball.display();
  
 }
 function keyPressed{
 	if (keyCode === UP_ARROW){
-		Matter.Body.applyForce(paperObject.Body,paperObject.Body.position,{x:85,y:-85});
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85});
 	}
 }
