5c4928923d826bae628ac051cbeacfd4988cf493
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-11-11 15:40:34 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,11 +1,12 @@
 class Ball{
-    constructor(x,y,width,height){
-        this.body = Matter.Bodies.circle(x,y,radious,{isStatic:true})
+    constructor(x,y,radius){
+        this.body = Matter.Bodies.circle(x,y,radius,{restitution:0.8})
         World.add(world,this.body);
-        this.width = width;
-        this.height = height;
+        this.radius = radius;
     }
     display(){
         fill("red");
+        ellipseMode(RADIUS);
+        ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
     }
 }
